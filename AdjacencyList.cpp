a4978435a2c3a82b9a60c88529b260e497b2874d
Bhavana <148469250+BhavanaK223@users.noreply.github.com>
2024-11-05 00:06:17 -0500

    Create AdjacencyList.cpp

---
+++ AdjacencyList.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include "AdjacencyList.h"
+
+using namespace std;
+
+//Constructor
+AdjacencyList::AdjacencyList() {
+
+}
+
+//Getters
+map<string, vector<string>> AdjacencyList::getList() {
+    return graph;
+}
+
+// prints the PageRank of all pages after p powerIterations in ascending
+// alphabetical order of webpages and rounding rank to two decimal places
+string AdjacencyList::PageRank(int n){
+    // optionally, store your output in a string/stringstream and then return it from this function after printing so that it is easier to test with Catch
+    string result;
+
+    // do your page rank
+
+    cout << result;
+    return result;
+}
